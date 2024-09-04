8f0e49c6ff2413ad16cd30449b4f8c9a74a702c6
Francis <144322947+Oniworks@users.noreply.github.com>
2024-09-04 13:36:59 +0300

    Add files via upload

---
+++ PU9.py
@@ -0,0 +1,11 @@
+class store:
+    pass
+
+alternative_rocks = store()
+isabelles_ices = store()
+
+alternative_rocks.store_name = "Alternative rocks"
+isabelles_ices.store_name = "Isabelle\'s ices"
+
+print(alternative_rocks.store_name)
+print(isabelles_ices.store_name)
